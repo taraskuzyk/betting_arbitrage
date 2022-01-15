28050143a8d5df30822d79d1b0e974342b161a96
Taras Kuzyk <taras@salesmeetings.ai>
2022-01-15 00:43:56 -0700

    added sport enum to db

--- betting_arbitrage/database/orm/bet.py
+++ betting_arbitrage/database/orm/bet.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 
-from sqlalchemy import String, Column, Float, Enum, DateTime, Integer
+from sqlalchemy import String, Column, Float, Enum, DateTime, Integer, Date
 
 from database.orm.base import Base
+from database.orm.sport import Sport
 from database.orm.website import Website
 
 
@@ -10,6 +11,7 @@ class Bet(Base):
     __tablename__ = "bets"
 
     id = Column(Integer, primary_key=True, nullable=False)
+
     team_1_name = Column(String(50), nullable=False)
     team_1_odds = Column(Float, nullable=False)
 
@@ -18,6 +20,9 @@ class Bet(Base):
 
     draw_odds = Column(Float, nullable=True)
 
+    match_date = Column(Date)
+
+    sport = Column(Enum(Sport), nullable=False)
     website = Column(Enum(Website), nullable=False)
 
     datetime_scraped = Column(DateTime, nullable=False, default=datetime.now())
---
+++ betting_arbitrage/database/orm/sport.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class Sport(Enum):
+    csgo = "csgo"
+    dota2 = "dota2"
+    lol = "lol"
+    valorant = "valorant"
+    starcraft2 = "starcraft2"
+    rocket_league = "rocket_league"
